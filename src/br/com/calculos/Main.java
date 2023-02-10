c98a199b610af33338219af7195034f038c9c03d
apla77 <verocidade77@gmail.com>
2023-02-09 21:45:20 -0300

    Novo commit de teste

--- src/br/com/calculos/Main.java
+++ src/br/com/calculos/Main.java
@@ -2,9 +2,10 @@ package br.com.calculos;
 
 public class Main {
 
+	// token Git: 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
 	public static void main(String[] args) {
 		System.out.println("Ola Galerinha!!!");
-
+		System.out.println("Novo commit!!!");
 	}
 
 }
