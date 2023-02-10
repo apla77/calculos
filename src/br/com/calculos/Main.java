c798040f4668bc6482f0a3a59364aacb84408254
apla77 <verocidade77@gmail.com>
2023-02-09 23:39:23 -0300

    Update interface operações

--- src/br/com/calculos/Main.java
+++ src/br/com/calculos/Main.java
@@ -2,14 +2,15 @@ package br.com.calculos;
 
 public class Main {
 
-	// token master : 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
-	// token staging: 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
+	// token master :   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
+	// token staging:   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
+	// token operações: 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
 	public static void main(String[] args) {
 		
 		final double valor; 
 		Operacoes op = new Calculos();
 		System.out.println("Ola Galerinha!!!");
-		System.out.println(op.soma(3, 7));
+		System.out.println("O total de 3 + 7 = " + op.soma(3, 7));
 	}
 
 
